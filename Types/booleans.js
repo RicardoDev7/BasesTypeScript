5c5d609dd90549eb7a812f5e6d1643fa2b802edc
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-12-12 22:57:54 -0500

    feat: introduccion a booleanos en TS

---
+++ Types/booleans.js
@@ -0,0 +1,10 @@
+"use strict";
+(() => {
+    let isSuperman = true;
+    let isBatman = false;
+    console.log({ isSuperman });
+    console.log({ isBatman });
+    let message = isSuperman ? "I'm Superman" : "I'm not Superman";
+    console.log({ message });
+})();
+//# sourceMappingURL=booleans.js.map
---
+++ Types/booleans.ts
@@ -0,0 +1,8 @@
+(() => {
+    let isSuperman: boolean = true;
+    let isBatman: boolean = false;
+    console.log({isSuperman});
+    console.log({isBatman});
+    let message: string = isSuperman ? "I'm Superman" : "I'm not Superman";
+    console.log({message});
+})();
