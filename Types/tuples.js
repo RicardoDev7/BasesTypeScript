423189f83be139fc8ac614d4b7c7d9900360e342
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-12-13 23:52:56 -0500

    feat: tuples en TS

---
+++ Types/tuples.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const hero = ['Captian America', 100, true];
+    const hero1 = ['Iron Man', 200];
+    console.log(hero);
+    console.log(hero1);
+})();
+//# sourceMappingURL=tuples.js.map
---
+++ Types/tuples.ts
@@ -0,0 +1,6 @@
+(() => {
+    const hero: [string, number, boolean] = ['Captian America', 100, true];
+    const hero1: [string, number] = ['Iron Man', 200];
+    console.log(hero);
+    console.log(hero1);
+})();
