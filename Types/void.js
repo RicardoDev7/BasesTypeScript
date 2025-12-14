d28c975c1f3590c72f835f36c53d9358dc9a44c0
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-12-14 00:04:26 -0500

    feat: return void in TS

---
+++ Types/void.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    function callBatman() {
+        return;
+    }
+    const callSuperman = () => { };
+    const a = callBatman();
+    console.log(a);
+    const b = callSuperman();
+    console.log(b);
+})();
+//# sourceMappingURL=void.js.map
