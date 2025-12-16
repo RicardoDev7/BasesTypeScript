8ebd03eccd2366245bb6f837ec7cd7508bee3e1a
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-12-15 22:44:14 -0500

    feat: objetos literales en TS

---
+++ Objects/objects.ts
@@ -0,0 +1,14 @@
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'Viaje en el tiempo']
+    }
+    console.log(flash);
+    flash = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Super fuerza']
+    }
+    console.log(flash);
+})();
