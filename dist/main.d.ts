f39e1145bf2537f3d81bf676ef7f66d953ca6e35
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-12-18 22:56:18 -0500

    feat: variables let y const

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,8 +1,1 @@
-declare const sumar: (a: number, b: number) => number;
-declare const contar: (heroes: Array<string>) => number;
-declare const superHeroes: string[];
-declare const llamarBatman: (llamar?: boolean) => void;
-declare const unirheroes: (...personas: string[]) => string;
-declare const noHaceNada: (numero: number, texto: string, booleano: boolean, arreglo: Array<string>) => void;
-declare let noHaceNadaTampoco: (a: number, b: string, c: boolean, d: Array<string>) => void;
 //# sourceMappingURL=main.d.ts.map
---
+++ src/es6/let-var.ts
@@ -0,0 +1,10 @@
+(() => {
+    let a = 'let';
+    console.log(a);
+
+    const name: string = 'Ricardo';
+    const getName = () : void => {
+        console.log(name);
+    }
+    getName();
+})();
