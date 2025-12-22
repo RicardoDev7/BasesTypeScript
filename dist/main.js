8a6f6f413093ae401710e0f7acb02d793ba081a0
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-12-21 23:52:38 -0500

    feat: metodos en clases

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,9 @@
         team;
         realName;
         static averageAge = 35;
+        static getAverageAge() {
+            return this.name;
+        }
         constructor(name, team, realName) {
             this.name = name;
             this.team = team;
@@ -13,9 +16,14 @@
             this.team = team;
             this.realName = realName ?? '';
         }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     const antman = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman.realName);
     console.log(Avenger.averageAge);
+    console.log(antman.bio());
+    console.log(Avenger.getAverageAge());
 })();
 //# sourceMappingURL=main.js.map
--- src/classes/basic.ts
+++ src/classes/basic.ts
@@ -4,6 +4,10 @@
      */
     class Avenger {
         static averageAge: number = 35;
+        static getAverageAge() : string {
+            //! El this apunta al scope padre
+            return this.name;
+        }
 
         constructor(
             private name: string, 
@@ -15,9 +19,15 @@
             this.team = team;
             this.realName = realName ?? '';
         }
+
+        public bio() : string {
+            return `${this.name} (${this.team})`;
+        }
     }
 
     const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman.realName);
     console.log(Avenger.averageAge);
+    console.log(antman.bio());
+    console.log(Avenger.getAverageAge());
 })();
